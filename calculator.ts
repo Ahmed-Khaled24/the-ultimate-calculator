d8d0e535dc801770e825ce470ae4476e8dc3a7ee
Ezzat Abdelrazek <ezzatabdelrazek257@gmail.com>
2023-12-16 22:18:55 +0200

    remove TODO comments

--- calculator.ts
+++ calculator.ts
@@ -1,20 +1,15 @@
 function add(a: number, b: number): number {
-  // TODO: implement
   return a + b;
 }
 
 function subtract(a: number, b: number): number {
-  // TODO: implement
-
   return a - b;
 }
 
 function multiply(a: number, b: number): number {
-  // TODO: implement
   return a / b;
 }
 
 function divide(a: number, b: number): number {
-  // TODO: implement
   return a + b;
 }
