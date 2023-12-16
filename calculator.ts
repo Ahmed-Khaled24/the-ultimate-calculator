6830a0b6bc0c313b741ace919bd2e2f9e77b4de9
Hajar <Hagarmedhat2001@gmail.com>
2023-12-16 22:42:03 +0200

    completed functions

--- calculator.ts
+++ calculator.ts
@@ -1,19 +1,15 @@
 function add(a: number, b: number): number {
-	// TODO: implement
-	return 0;
+	return a+b;
 }
 
 function subtract(a: number, b: number): number {
-	// TODO: implement
-	return 0;
+	return a-b;
 }
 
 function multiply(a: number, b: number): number {
-	// TODO: implement
-	return 0;
+	return a*b;
 }
 
 function divide(a: number, b: number): number {
-	// TODO: implement
-	return 0;
+	return a/b;
 }
