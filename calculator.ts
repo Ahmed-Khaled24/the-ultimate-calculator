94e13c573278e8d02c9fe763c6232b76395f41b7
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-16 19:14:30 +0200

    initial commit

---
+++ calculator.ts
@@ -0,0 +1,19 @@
+function add(a: number, b: number): number {
+	// TODO: implement
+	return 0;
+}
+
+function subtract(a: number, b: number): number {
+	// TODO: implement
+	return 0;
+}
+
+function multiply(a: number, b: number): number {
+	// TODO: implement
+	return 0;
+}
+
+function divide(a: number, b: number): number {
+	// TODO: implement
+	return 0;
+}
